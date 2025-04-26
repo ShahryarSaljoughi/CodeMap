351a44e27b08c338173040cce61a3564e1bfa602
shahryar saljoughi <s.shahryar75@gmail.com>
2025-04-27 03:20:35 +0330

    Add GitIgnoreService and TreeBuilder classes for handling .gitignore rules and directory structure

---
+++ CodeMap.Core/GitIgnoreService.cs
@@ -0,0 +1,74 @@
+﻿namespace CodeMap.Core;
+
+public class GitIgnoreService
+{
+    private string? GitIgnorePath { get; set; }
+    private Ignore.Ignore Ignore { get; set; }
+    private bool IsInitialized { get; set; }
+    public GitIgnoreService(string ignoreFilePath)
+    {
+        GitIgnorePath =
+            File.Exists(ignoreFilePath) ?
+                ignoreFilePath
+                : Directory.Exists(ignoreFilePath) ?
+                    Directory.GetFiles(ignoreFilePath).FirstOrDefault(p => p.Contains(".gitignore"))
+                    : null;
+    }
+
+    private async Task Initialize()
+    {
+        Ignore = new Ignore.Ignore();
+        if (!string.IsNullOrWhiteSpace(GitIgnorePath))
+        {
+            var rules = await File.ReadAllLinesAsync(GitIgnorePath!);
+            Ignore.Add(rules);
+        }
+
+        IsInitialized = true;
+    }
+
+    public async Task<List<string>> ApplyGitIgnoreAsync(List<string> paths)
+    {
+        if (!IsInitialized) await Initialize();
+        if (string.IsNullOrWhiteSpace(GitIgnorePath)) return paths;
+
+        paths = paths.Select(p => p.Replace(@"\", "/")).ToList();
+        return paths.Where(p => !Ignore.IsIgnored(p) && !BinaryDetector.IsBinary(p)).ToList();
+    }
+}
+
+public class BinaryDetector
+{
+    public static bool IsBinary(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path)) return false;
+        if (!File.Exists(path) && Directory.Exists(path)) return false; // it's a directory
+
+        using (StreamReader stream = new StreamReader(path))
+        {
+            int ch;
+            while ((ch = stream.Read()) != -1)
+            {
+                if (IsControlChar(ch))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static bool IsControlChar(int ch)
+    {
+        return (ch > Chars.NUL && ch < Chars.BS)
+               || (ch > Chars.CR && ch < Chars.SUB);
+    }
+
+    public static class Chars
+    {
+        public static char NUL = (char)0; // Null char
+        public static char BS = (char)8; // Back Space
+        public static char CR = (char)13; // Carriage Return
+        public static char SUB = (char)26; // Substitute
+    }
+}
---
+++ CodeMap.Core/TreeBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeMap.Core;
+
+public class TreeBuilder(GitIgnoreService gitIgnoreService)
+{
+    public async Task<Node> BuildTree(string rootPath)
+    {
+        var isFile = File.Exists(rootPath);
+        rootPath = NormalizeDirectoryPath(rootPath);
+        var root = new Node()
+        {
+            Type = isFile ? NodeType.File : NodeType.Directory,
+            FullPath = rootPath,
+            Name = Path.GetDirectoryName(rootPath)
+        };
+        root.Children.AddRange(await BuildTreeChildren(rootPath));
+        return root;
+    }
+
+    private async Task<List<Node>> BuildTreeChildren(string rootPath)
+    {
+        var files = Directory.GetFiles(rootPath).ToList();
+        files = await gitIgnoreService.ApplyGitIgnoreAsync(files);
+        var fileNodes = files.Select(f =>
+        {
+            var node = new Node()
+            {
+                Type = NodeType.File,
+                FullPath = f,
+                Name = Path.GetFileName(f),
+            };
+            return node;
+        });
+        var directories = Directory.GetDirectories(rootPath).Select(NormalizeDirectoryPath).ToList();
+        directories = await gitIgnoreService.ApplyGitIgnoreAsync(directories);
+        var directoryNodes = directories.Select(d =>
+        {
+            var node = new Node()
+            {
+                Type = NodeType.Directory,
+                Name = Path.GetDirectoryName(d) ?? d,
+                FullPath = d,
+            };
+            return node;
+        }).ToList();
+
+        foreach (var directoryNode in directoryNodes)
+        {
+            directoryNode.Children.AddRange(await BuildTreeChildren(directoryNode.FullPath));
+        }
+        return fileNodes.Concat(directoryNodes).ToList();
+    }
+
+    private string NormalizeDirectoryPath(string path)
+    {
+        path = path.Replace(@"\", "/");
+        if (!DoesEndInSlash(path))
+            path = $"{path}/";
+        return path;
+    }
+
+    bool DoesEndInSlash(string path)
+    {
+        var endsInSlashPattern = @"^.*/$";
+        return new Regex(endsInSlashPattern).IsMatch(path);
+    }
+
+}
